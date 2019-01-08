c2ee2015295dffe6f1689226e7b047419ea112b4
vinicio@codefellows.com <vinicio@codefellows.com>
2019-01-08 11:24:11 -0800

    prepping for day 03

---
+++ dotnet-prep/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
